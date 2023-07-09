8a13b06551a150e09ae807c4c0f73eb2cf9940fd
Kamal <99984032+Kamal-M-Ali@users.noreply.github.com>
2023-07-09 07:48:04 -0400

    Update Featured.js

--- src/UserHome/Featured.js
+++ src/UserHome/Featured.js
@@ -8,6 +8,7 @@ export default function Featured(props)
             {
                 title : "The Great Gatsby",
                 desc : "This is a book description temporary placeholder.",
+                genre : "Young Adult",
                 author : "F-Scott Fitzgerald",
                 cover : "https://s26162.pcdn.co/wp-content/uploads/2018/02/gatsby-original2.jpg",
                 rating : 4.3,
@@ -17,6 +18,7 @@ export default function Featured(props)
             {
                 title : "Invisible Man",
                 desc : "This is a book description temporary placeholder.",
+                genre : "Young Adult",
                 author : "Ralph Ellison",
                 cover : "https://s26162.pcdn.co/wp-content/uploads/2018/10/invisibleman-1509049531-640x996.jpg",
                 rating : 4.5,
@@ -26,6 +28,7 @@ export default function Featured(props)
             {
                 title : "Fahrenheit 451",
                 desc : "This is a book description temporary placeholder.",
+                genre : "Young Adult",
                 author : "Ray Bradbury",
                 cover : "https://s26162.pcdn.co/wp-content/uploads/2021/10/lf-1-1.jpg",
                 rating : 3.7,
@@ -35,6 +38,7 @@ export default function Featured(props)
             {
                 title : "To Kill a Mockingbird",
                 desc : "This is a book description temporary placeholder.",
+                genre : "Young Adult",
                 author : "Harper Lee",
                 cover : "https://s26162.pcdn.co/wp-content/uploads/2021/10/To_Kill_a_Mockingbird_first_edition_cover.jpg",
                 rating : 4.2,
@@ -44,6 +48,7 @@ export default function Featured(props)
             {
                 title : "Kindred",
                 desc : "This is a book description temporary placeholder.",
+                genre : "Young Adult",
                 author : "Ocatvia E. Butler",
                 cover : "https://m.media-amazon.com/images/I/91pIF5zxBVL._AC_UF1000,1000_QL80_.jpg",
                 rating : 4.6,
@@ -55,6 +60,7 @@ export default function Featured(props)
             {
                 title : "Five Survive",
                 desc : "This is a book description temporary placeholder.",
+                genre : "Young Adult",
                 author : "Holly Jackson",
                 cover : "https://storage.googleapis.com/du-prd/books/images/9780593374160.jpg",
                 rating : 4.1,
@@ -64,6 +70,7 @@ export default function Featured(props)
             {
                 title : "Solitaire",
                 desc : "This is a book description temporary placeholder.",
+                genre : "Young Adult",
                 author : "Alice Oseman",
                 cover : "https://storage.googleapis.com/du-prd/books/images/9781339016238.jpg",
                 rating : 4.3,
@@ -73,6 +80,7 @@ export default function Featured(props)
             {
                 title : "Divine Rivals",
                 desc : "This is a book description temporary placeholder.",
+                genre : "Young Adult",
                 author : "Rebecca Ross",
                 cover : "https://storage.googleapis.com/du-prd/books/images/9781250857439.jpg",
                 rating : 4.7,
@@ -82,6 +90,7 @@ export default function Featured(props)
             {
                 title : "The first to Die at the End",
                 desc : "This is a book description temporary placeholder.",
+                genre : "Young Adult",
                 author : "Adam Silvera",
                 cover : "https://storage.googleapis.com/du-prd/books/images/9780063240803.jpg",
                 rating : 4.4,
@@ -91,6 +100,7 @@ export default function Featured(props)
             {
                 title : "Lightlark",
                 desc : "This is a book description temporary placeholder.",
+                genre : "Young Adult",
                 author : "Alex Aster",
                 cover : "https://storage.googleapis.com/du-prd/books/images/9781419760860.jpg",
                 rating : 3.9,
@@ -100,6 +110,7 @@ export default function Featured(props)
             {
                 title : "Warrior Girl Unearthed",
                 desc : "This is a book description temporary placeholder.",
+                genre : "Young Adult",
                 author : "Angeline Boulley",
                 cover : "https://storage.googleapis.com/du-prd/books/images/9781250766588.jpg",
                 rating : 4.6,
@@ -119,6 +130,8 @@ export default function Featured(props)
             rating={book.rating}
             price={book.price}
             isbn={book.isbn}
+            desc={book.desc}
+            genre={book.genre}
         />
     )}</div>
     );
