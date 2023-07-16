3b39cc8477f0231279cadaed8ff9e654d3e6e219
Kamal <99984032+Kamal-M-Ali@users.noreply.github.com>
2023-07-16 16:42:11 -0400

    add email regex/password length res

--- bookstore-backend/src/main/java/com/bookstore/system/controller/CustomerController.java
+++ bookstore-backend/src/main/java/com/bookstore/system/controller/CustomerController.java
@@ -17,11 +17,26 @@ public class CustomerController {
     private CustomerRepository customerRepository;
 
     @PostMapping("/api/signup")
-    @PostMapping("/api/signup")
     ResponseEntity<String> newCustomer(@Valid @RequestBody Customer newCustomer) {
         newCustomer.setCustomerState(Customer.CUSTOMER_STATE.INACTIVE);
         // TODO: add password encryption
+        if (newCustomer.getPassword().length() < 6)
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body("Password too short");
+
         // TODO: send verification email
+
+        // email validation
+        // regex source: https://www.baeldung.com/java-email-validation-regex
+        if (!Pattern.compile("^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$")
+                .matcher(newCustomer.getEmail())
+                .matches())
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body("Invalid email");
+
+        // account created ok
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body("Account Created");
