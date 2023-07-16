9fb8271f7609cd947d6f721f3fd19e41e8f652f8
Kamal <99984032+Kamal-M-Ali@users.noreply.github.com>
2023-07-15 21:54:59 -0400

    Update BookController.java

--- bookstore-backend/src/main/java/com/bookstore/system/controller/BookController.java
+++ bookstore-backend/src/main/java/com/bookstore/system/controller/BookController.java
@@ -1,5 +1,28 @@
 package com.bookstore.system.controller;
 
-public class BookController {
+import com.bookstore.system.exception.BookNotFoundException;
+import com.bookstore.system.model.Book;
+import com.bookstore.system.model.Customer;
+import com.bookstore.system.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@RestController
+public class BookController {
+    @Autowired
+    private BookRepository bookRepository;
+
+    @GetMapping("/api/book/:{id}")
+    Book getBook(@PathVariable Integer id) {
+        return bookRepository.findById(id).orElseThrow(() -> new BookNotFoundException(id));
+    }
+
+    @GetMapping("/api/books")
+    List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
 }
