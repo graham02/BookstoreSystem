3f774f1785627644599d1401304039e3b5f0aa06
Kamal <99984032+Kamal-M-Ali@users.noreply.github.com>
2023-07-15 21:40:25 -0400

    sql table for sample books

---
+++ sample_books.sql
@@ -0,0 +1,200 @@
+INSERT INTO `bookstore`.`book`
+(`id`,
+`author`,
+`book_type`,
+`category`,
+`coverurl`,
+`description`,
+`in_stock`,
+`isbn`,
+`price`,
+`publication_year`,
+`publisher`,
+`rating`,
+`title`)
+VALUES
+(1,
+"F. Scott Fitzgerald",
+0,
+"Young Adult",
+"https://s26162.pcdn.co/wp-content/uploads/2018/02/gatsby-original2.jpg",
+"The Great Gatsby is considered F. Scott Fitzgerald’s magnum opus, exploring themes of decadence, idealism, social stigmas, patriarchal norms, and the deleterious effects of unencumbered wealth in capitalistic society, set against the backdrop of the Jazz Age and the Roaring Twenties. At its heart, it’s a cautionary tale, a revealing look into the darker side to the American Dream.
+
+“When I came back from the East last autumn I felt that I wanted the world to be in uniform and at a sort of moral attention forever; I wanted no more riotous excursions with privileged glimpses into the human heart. Only Gatsby, the man who gives his name to this book, was exempt from my reaction—Gatsby who represented everything for which I have an unaffected scorn. If personality is an unbroken series of successful gestures, then there was something gorgeous about him, some heightened sensitivity to the promises of life, as if he were related to one of those intricate machines that register earthquakes ten thousand miles away. This responsiveness had nothing to do with that flabby impressionability which is dignified under the name of the “creative temperament”—it was an extraordinary gift for hope, a romantic readiness such as I have never found in any other person and which it is not likely I shall ever find again. No—Gatsby turned out all right at the end; it is what preyed on Gatsby, what foul dust floated in the wake of his dreams that temporarily closed out my interest in the abortive sorrows and short-winded elations of men...”",
+100,
+"979-8351145013",
+5,
+2022,
+"Independently published",
+4.7,
+"The Great Gatsby"),
+
+(2,
+"Ralph Ellison",
+0,
+"Young Adult",
+"https://s26162.pcdn.co/wp-content/uploads/2018/10/invisibleman-1509049531-640x996.jpg",
+"NATIONAL BOOK AWARD WINNER • NATIONAL BESTSELLER • In this deeply compelling novel and epic milestone of American literature, a nameless narrator tells his story from the basement lair of the Invisible Man he imagines himself to be.",
+100,
+"978-0679732761",
+12.51,
+1995,
+"Vintage Books",
+4.5,
+"Invisible Man"),
+
+(3,
+"Ray Bradbury",
+0,
+"Young Adult",
+"https://s26162.pcdn.co/wp-content/uploads/2021/10/lf-1-1.jpg",
+"Guy Montag is a fireman. His job is to destroy the most illegal of commodities, the printed book, along with the houses in which they are hidden. Montag never questions the destruction and ruin his actions produce, returning each day to his bland life and wife, Mildred, who spends all day with her television “family.” But when he meets an eccentric young neighbor, Clarisse, who introduces him to a past where people didn’t live in fear and to a present where one sees the world through the ideas in books instead of the mindless chatter of television, Montag begins to question everything he has ever known.",
+100,
+"978-1451673319",
+14.57,
+2012,
+"Simon & Schuster",
+4.6,
+"Fahrenheit 451"),
+
+(4,
+"Harper Lee",
+0,
+"Young Adult",
+"https://s26162.pcdn.co/wp-content/uploads/2021/10/To_Kill_a_Mockingbird_first_edition_cover.jpg",
+"One of the most cherished stories of all time, To Kill a Mockingbird has been translated into more than forty languages, sold more than forty million copies worldwide, served as the basis for an enormously popular motion picture, and was voted one of the best novels of the twentieth century by librarians across the country. A gripping, heart-wrenching, and wholly remarkable tale of coming-of-age in a South poisoned by virulent prejudice, it views a world of great beauty and savage inequities through the eyes of a young girl, as her father—a crusading local lawyer—risks everything to defend a black man unjustly accused of a terrible crime.",
+100,
+"978-0060935467",
+8.89,
+2002,
+"Harper Perennial",
+4.7,
+"To Kill a Mockingbird"),
+
+(5,
+"Octavia E. Butler",
+0,
+"Young Adult",
+"https://m.media-amazon.com/images/I/91pIF5zxBVL._AC_UF1000,1000_QL80_.jpg",
+"From the New York Times bestselling author of Parable of the Sower and MacArthur “Genius” Grant, Nebula, and Hugo award winner 
+
+“I lost an arm on my last trip home. My left arm.”
+
+Dana’s torment begins when she suddenly vanishes on her 26th birthday from California, 1976, and is dragged through time to antebellum Maryland to rescue a boy named Rufus, heir to a slaveowner’s plantation. She soon realizes the purpose of her summons to the past: protect Rufus to ensure his assault of her Black ancestor so that she may one day be born. As she endures the traumas of slavery and the soul-crushing normalization of savagery, Dana fights to keep her autonomy and return to the present.",
+100,
+"978-0807083697",
+9.74,
+2003,
+"Beacon Press",
+4.6,
+"Kindred"),
+
+(6,
+"Holly Jackson",
+1,
+"Young Adult",
+"https://storage.googleapis.com/du-prd/books/images/9780593374160.jpg",
+"#1 NEW YORK TIMES BESTSELLER • From the author of the multimillion-copybestselling A Good Girl’s Guide to Murder series comes a new blockbuster thriller about a road trip that turns deadly.
+
+Eight hours. Six friends. Five survive.
+
+Red Kenny is on a road trip for spring break with five friends: Her best friend - the older brother - his perfect girlfriend - a secret crush - a classmate - and a killer. 
+
+When their RV breaks down in the middle of nowhere with no cell service, they soon realize this is no accident. They have been trapped by someone out there in the dark, someone who clearly wants one of them dead.
+
+With eight hours until dawn, the six friends must escape, or figure out which of them is the target. But is there a liar among them? Buried secrets will be forced to light and tensions inside the RV will reach deadly levels. Not all of them will survive the night. . . . 
+
+With edge-of-your-seat tension and a gripping mystery, Holly Jackson has written another instant classic!",
+100,
+"978-0593374160",
+14.39,
+2022,
+"Delacorte Press",
+4.3,
+"Five Survive"),
+
+(7,
+"Rebecca Ross",
+1,
+"Young Adult",
+"https://storage.googleapis.com/du-prd/books/images/9781250857439.jpg",
+"When two young rival journalists find love through a magical connection, they must face the depths of hell, in a war among gods, to seal their fate forever.
+
+After centuries of sleep, the gods are warring again. But eighteen-year-old Iris Winnow just wants to hold her family together. Her mother is suffering from addiction and her brother is missing from the front lines. Her best bet is to win the columnist promotion at the Oath Gazette.
+
+To combat her worries, Iris writes letters to her brother and slips them beneath her wardrobe door, where they vanish―into the hands of Roman Kitt, her cold and handsome rival at the paper. When he anonymously writes Iris back, the two of them forge a connection that will follow Iris all the way to the front lines of battle: for her brother, the fate of mankind, and love.
+
+Shadow and Bone meets Lore in Rebecca Ross's Divine Rivals, an epic enemies-to-lovers fantasy novel filled with hope and heartbreak, and the unparalleled power of love.",
+100,
+"978-1250857439",
+16.39,
+2023,
+"Wednesday Books",
+4.5,
+"Divine Rivals"),
+
+(8,
+"Adam Silvera",
+1,
+"Young Adult",
+"https://storage.googleapis.com/du-prd/books/images/9780063240803.jpg",
+"In this prequel to the bestselling phenomenon They Both Die at the End, two new strangers spend a life-changing day together after Death-Cast first makes their fateful calls. #1 New York Times bestseller!
+
+It’s the night before Death-Cast goes live, and there’s one question on everyone’s mind: Can Death-Cast actually predict when someone will die, or is it just an elaborate hoax?
+
+Orion Pagan has waited years for someone to tell him that he’s going to die. He has a serious heart condition, and he signed up for Death-Cast so he could know what’s coming.
+
+Valentino Prince is restarting his life in New York. He has a long and promising future ahead and he only registered for Death-Cast after his twin sister nearly died in a car accident.
+
+Orion and Valentino cross paths in Times Square and immediately feel a deep connection. But when the first round of End Day calls goes out, their lives are changed forever—one of them receives a call, and the other doesn’t. Though neither boy is certain how the day will end, they know they want to spend it together…even if that means their goodbye will be heartbreaking.
+
+Told with acclaimed author Adam Silvera’s signature bittersweet touch, this story celebrates the lasting impact that people have on each other and proves that life is always worth living to the fullest.",
+100,
+"978-0063240803",
+11.56,
+2022,
+"Quill Tree Books",
+4.6,
+"The First to Die at the End"),
+
+(9,
+"Alex Aster",
+2, 
+"Young Adult",
+"https://storage.googleapis.com/du-prd/books/images/9781419760860.jpg",
+"An instant #1 National Bestseller—soon to be a major motion picture. #BookTok phenomenon and award-winning author Alex Aster delivers readers a masterfully written, utterly gripping YA fantasy novel
+
+Welcome to the Centennial.
+
+Every 100 years, the island of Lightlark appears to host the Centennial, a deadly game that only the rulers of six realms are invited to play. The invitation is a summons—a call to embrace victory and ruin, baubles and blood. The Centennial offers the six rulers one final chance to break the curses that have plagued their realms for centuries. Each ruler has something to hide. Each realm’s curse is uniquely wicked. To destroy the curses, one ruler must die.
+
+Isla Crown is the young ruler of Wildling—a realm of temptresses cursed to kill anyone they fall in love with. They are feared and despised, and are counting on Isla to end their suffering by succeeding at the Centennial.
+
+To survive, Isla must lie, cheat, and betray…even as love complicates everything.",
+100,
+"978-1419760860",
+14.08,
+2022,
+"Harry N. Abrams",
+4.3,
+"Lightlark"),
+
+(10,
+"Angeline Boulley",
+1,
+"Young Adult",
+"https://storage.googleapis.com/du-prd/books/images/9781250766588.jpg",
+"#1 New York Times bestselling author of Firekeeper's Daughter Angeline Boulley takes us back to Sugar Island in this high-stakes thriller about the power of discovering your stolen history.
+
+Perry Firekeeper-Birch has always known who she is - the laidback twin, the troublemaker, the best fisher on Sugar Island. Her aspirations won't ever take her far from home, and she wouldn't have it any other way. But as the rising number of missing Indigenous women starts circling closer to home, as her family becomes embroiled in a high-profile murder investigation, and as greedy grave robbers seek to profit off of what belongs to her Anishinaabe tribe, Perry begins to question everything.
+
+In order to reclaim this inheritance for her people, Perry has no choice but to take matters into her own hands. She can only count on her friends and allies, including her overachieving twin and a charming new boy in town with unwavering morals. Old rivalries, sister secrets, and botched heists cannot - will not - stop her from uncovering the mystery before the ancestors and missing women are lost forever.
+
+Sometimes, the truth shouldn't stay buried.",
+100,
+"978-1250766588",
+15.99,
+2023,
+"Henry Holt and Co. (BYR)",
+4.6,
+"Warrior Girl Unearthed");
