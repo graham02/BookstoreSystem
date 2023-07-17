c37ae713ebcfd794b28a7ba699dc331422a870d4
Kamal <99984032+Kamal-M-Ali@users.noreply.github.com>
2023-07-16 20:49:30 -0400

    add verification email

--- bookstore-backend/src/main/java/com/bookstore/system/controller/CustomerController.java
+++ bookstore-backend/src/main/java/com/bookstore/system/controller/CustomerController.java
@@ -4,28 +4,31 @@ import com.bookstore.system.model.Cart;
 import com.bookstore.system.model.Customer;
 import com.bookstore.system.model.Login;
 import com.bookstore.system.repository.CustomerRepository;
+import com.bookstore.system.service.EmailService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import java.util.UUID;
 import java.util.regex.Pattern;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin
 public class CustomerController {
     @Autowired
     private CustomerRepository customerRepository;
 
+    @Autowired
+    EmailService emailService;
+
+
     @PostMapping("/api/signup")
     ResponseEntity<String> newCustomer(@Valid @RequestBody Customer newCustomer) {
         if (customerRepository.findByEmail(newCustomer.getEmail()) != null)
-            return ResponseEntity
-                    .status(HttpStatus.BAD_REQUEST)
-                    .body("Account already exists");
+            return ResponseEntity.badRequest().body("Account already exists");
 
         // new customers must be verified via email
         newCustomer.setCustomerState(Customer.CUSTOMER_STATE.INACTIVE);
@@ -35,32 +38,47 @@ public class CustomerController {
 
         // ensure password length > 6
         if (newCustomer.getPassword().length() < 6)
-            return ResponseEntity
-                    .status(HttpStatus.BAD_REQUEST)
-                    .body("Password too short");
+            return ResponseEntity.badRequest().body("Password too short");
 
         // encrypt their password
         BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
         newCustomer.setPassword(passwordEncoder.encode(newCustomer.getPassword()));
 
-        // TODO: send verification email
+        // send verification email
+        newCustomer.setConfirmationToken(UUID.randomUUID().toString());
+
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(newCustomer.getEmail());
+        mailMessage.setSubject("Complete Registration!");
+        mailMessage.setText("To confirm your account, please click here : "
+                +"http://localhost:8080/verify-account?token="+newCustomer.getVerificationToken());
+        emailService.sendEmail(mailMessage);
 
         // email validation
         // regex source: https://www.baeldung.com/java-email-validation-regex
         if (!Pattern.compile("^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$")
                 .matcher(newCustomer.getEmail())
                 .matches())
-            return ResponseEntity
-                    .status(HttpStatus.BAD_REQUEST)
-                    .body("Invalid email");
+            return ResponseEntity.badRequest().body("Invalid email");
 
         // save customer in database
         customerRepository.save(newCustomer);
 
         // account creation success
-        return ResponseEntity
-                .status(HttpStatus.OK)
-                .body("Account Created");
+        return ResponseEntity.ok().body("Account created");
+    }
+
+    @GetMapping("/verify-account")
+    public ResponseEntity<String> confirmEmail(@RequestParam("token") String verificationToken) {
+        Customer customer = customerRepository.findByVerificationToken(verificationToken);
+
+        if(customer != null)
+        {
+            customer.setCustomerState(Customer.CUSTOMER_STATE.ACTIVE);
+            customerRepository.save(customer);
+            return ResponseEntity.ok("Account has been activated");
+        }
+        return ResponseEntity.badRequest().body("Account doesn't exist");
     }
 
     @PostMapping("/api/login")
