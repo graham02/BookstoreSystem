fb7193998e807d0421dbeca86290718055777340
PixlePixle <67672793+PixlePixle@users.noreply.github.com>
2023-07-17 01:29:18 -0400

    Add Admin lookup

--- bookstore-backend/src/main/java/com/bookstore/system/controller/AdminController.java
+++ bookstore-backend/src/main/java/com/bookstore/system/controller/AdminController.java
@@ -1,6 +1,28 @@
 package com.bookstore.system.controller;
 
-import com.bookstore.system.model.Admin;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.bookstore.system.model.Admin;
+import com.bookstore.system.repository.AdminRepository;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class AdminController {
+    @Autowired
+    private AdminRepository adminRepository;
+
+    @GetMapping("/api/admin")
+    public ResponseEntity<?> getAdminProfile(@RequestParam("email") String email) {
+        Admin admin = adminRepository.findByEmail(email);
+
+        if (admin != null)
+            return ResponseEntity.ok().body(admin);
+        return ResponseEntity.badRequest().body("Invalid Credentials");
+    }
 }
--- bookstore-backend/src/main/java/com/bookstore/system/repository/AdminRepository.java
+++ bookstore-backend/src/main/java/com/bookstore/system/repository/AdminRepository.java
@@ -1,8 +1,12 @@
 package com.bookstore.system.repository;
 import com.bookstore.system.model.Admin;
+import com.bookstore.system.model.Customer;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AdminRepository extends JpaRepository<Admin, Integer> {
 
+
     Admin findByEmail(String email);
+    Admin findByVerificationToken(String verificationToken);
 }
