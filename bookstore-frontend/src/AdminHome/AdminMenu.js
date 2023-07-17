a40c636c13e773a3ab1f1f12eef691afe5ecb9bc
Kamal <99984032+Kamal-M-Ali@users.noreply.github.com>
2023-07-17 18:23:07 -0400

    switch adminmenu functionality to admin.js

--- bookstore-frontend/src/AdminHome/AdminMenu.js
+++ bookstore-frontend/src/AdminHome/AdminMenu.js
@@ -1,34 +1,12 @@
 import './AdminMenu.css';
 import { Link, useNavigate } from 'react-router-dom';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
 
-export default function Nagivation()
-{  
+export default function AdminMenu()
+{
     const key = 'auth';
     const navigate = useNavigate();
-    const [admin, setAdminStatus] = useState(false);
 
-    const API = 'http://localhost:8080/exists/admin';
-
-
-    function checkAdminStatus() {
-        axios.get(API, 
-            { params: {token: localStorage.getItem(key) || sessionStorage.getItem(key)}
-        }).then((res) => {
-            console.log("You're an admin!");
-            setAdminStatus(true);
-        }).catch((err) => {
-            console.log(err);
-            setAdminStatus(false);
-            navigate(-1);
-        });
-    }
-    
-    // eslint-disable-next-line
-    useEffect(checkAdminStatus, []);
-
-    return admin ?(<>
+    return (<>
     <div className='nav-bar'>
         <p className='home-button'>
             <Link className='nav-link' to='/Admin'>Home</Link>
@@ -40,14 +18,5 @@ export default function Nagivation()
     <div className='admin-banner'>
         <h1>Admin Menu</h1>
     </div>
-    </>)
-    :
-    (<>
-        <div className='nav-bar'>
-        </div>
-        <div className='admin-banner'>
-        <h1>Unauthorized</h1>
-    </div>
-    </>)
-    ;
+    </>);
 }
